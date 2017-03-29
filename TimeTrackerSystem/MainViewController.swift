ca303d781057ceeadd8d44b038af9b2c7c78c4bd
Anil Saini <Anil@Anils-iMac.local>
2017-03-29 12:18:00 +0400

    Sidebar navigator

---
+++ TimeTrackerSystem/MainViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MainViewController.swift
+//  TimeTrackerSystem
+//
+//  Created by Anil Saini on 3/15/17.
+//  Copyright © 2017 saini.com. All rights reserved.
+//
+
+import UIKit
+
+//extension UIViewController
+//{
+//    @nonobjc func revealViewController() {
+//        self.performSelector(Selector("revealViewController"))
+//    }
+//}
+
+class MainViewController: UIViewController {
+
+    @IBOutlet var sidebarButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "News"
+        if let revealVC:SWRevealViewController = self.revealViewController() {
+            self.sidebarButton.target = revealVC
+            self.sidebarButton.action = "revealToggle:"
+            //self.sidebarButton.targetForAction("revealToggle:", withSender: self)
+            self.view.addGestureRecognizer(revealVC.panGestureRecognizer())
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TimeTrackerSystemTests/TimeTrackerSystemTests.swift
@@ -0,0 +1,36 @@
+//
+//  TimeTrackerSystemTests.swift
+//  TimeTrackerSystemTests
+//
+//  Created by Anil Saini on 3/15/17.
+//  Copyright © 2017 saini.com. All rights reserved.
+//
+
+import XCTest
+@testable import TimeTrackerSystem
+
+class TimeTrackerSystemTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
